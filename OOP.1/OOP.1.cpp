cc1a27e3e8fe75a47be278fc5accbbb656457b15
jaddu17 <shafajaddu@gmail.com>
2024-04-23 09:18:04 +0700

    menampilkan methode untuk input

--- OOP.1/OOP.1.cpp
+++ OOP.1/OOP.1.cpp
@@ -6,4 +6,11 @@ class Mahasiswa {
 		string nama;
 		int umur;
 		string jurusan;
+
+
+		void input() {
+			cout << "Nama: " << nama << endl;
+			cout << "Umur: " << umur << endl;
+			cout << "Jurusan: " << jurusan << endl;
+		}
 };
